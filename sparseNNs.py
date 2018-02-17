905e4ec30562864f17ec874dc11e517a761f9a1f
Nicholas Dwork <ndwork@hotmail.com>
2018-02-17 12:06:44 -0800

    Made it so that only layers with parameters are in the init function.

--- sparseNNs.py
+++ sparseNNs.py
@@ -56,18 +56,14 @@ class Net(nn.Module):
   def __init__(self):
     super(Net, self).__init__()
     self.conv1 = nn.Conv2d(3, 6, 5)
-    self.pool  = nn.MaxPool2d(2, 2)  # parameter free
     self.conv2 = nn.Conv2d(6, 16, 5)
     self.fc1 = nn.Linear(16 * 5 * 5, 120)
     self.fc2 = nn.Linear(120, 84)
     self.fc3 = nn.Linear(84, 10)
 
   def forward(self, x):
-    x = self.pool(F.relu(self.conv1(x)))
-    #x = F.max_pool2d(F.relu(self.conv1(x)), 2)  # equivalent to line above
-      # this makes it so only the layers withparameters are in the init function
-    x = self.pool(F.relu(self.conv2(x)))
-    #x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+    x = F.max_pool2d(F.relu(self.conv1(x)), 2, 2)  # equivalent to the line above
+    x = F.max_pool2d(F.relu(self.conv2(x)), 2, 2)  # equivalent to the line above
     x = x.view(-1, 16 * 5 * 5)  # converts matrix to vector
     x = F.relu(self.fc1(x))
     x = F.relu(self.fc2(x))
