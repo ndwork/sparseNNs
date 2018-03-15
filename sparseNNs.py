6a1e1107ad4a1241b60f2f66087c031ad318193a
Nicholas Dwork <ndwork@hotmail.com>
2018-03-15 15:53:41 -0700

    Fixing another bug related to gpu stuff.

--- sparseNNs.py
+++ sparseNNs.py
@@ -554,9 +554,10 @@ def trainWithStochProxGradDescent_regL2L1Norm( net, criterion, params, learningR
       mainLoss = criterion( outputs, labels )
       regLoss = 0
       for thisMod in net.modules():
-        neurWeight = thisMod.weight.data.cpu().numpy()
-        neurBias = thisMod.bias.data.cpu().numpy()
-        regLoss = regLoss + np.square( np.sum( neurWeight * neurWeight ) + np.sum( neurBias * neurBias ) )
+        if hasattr( thisMod, 'weight' ):
+          neurWeight = thisMod.weight.data.cpu().numpy()
+          neurBias = thisMod.bias.data.cpu().numpy()
+          regLoss = regLoss + np.square( np.sum( neurWeight * neurWeight ) + np.sum( neurBias * neurBias ) )
       regLoss = regLoss * regParam/nWeights
       loss = mainLoss + regLoss
       costs[k] = mainLoss.data[0] + regLoss
