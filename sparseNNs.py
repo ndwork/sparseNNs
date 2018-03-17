8fa35eff2aeb33a21b18673e25430d50354f8d8b
Nicholas Dwork <ndwork@hotmail.com>
2018-03-17 14:20:09 -0700

    Searching hyperparameters

--- sparseNNs.py
+++ sparseNNs.py
@@ -1084,12 +1084,12 @@ class Params:
   momentum = 0.0
   nBatches = 1000000
   nEpochs = 100
-  printEvery = 5
+  printEvery = 50
   regParam_normL1 = 1e3
   regParam_normL2L1 = 1e3
   regParam_normL2Lhalf = 1e3
   seed = 1
-  showAccuracyEvery = 50
+  showAccuracyEvery = 500
   shuffle = False  # Shuffle the data in each minibatch
   alpha = 0.8
   s = 1.25  # Step size scaling parameter (must be greater than 1)
